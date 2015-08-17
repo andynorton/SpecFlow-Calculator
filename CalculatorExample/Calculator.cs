d60b5e03735c09d6a1f46d7d811829343518367b
Andrew Norton <andyjnorton@mac.com>
2015-08-17 15:20:29 +0100

    Added files

---
+++ CalculatorExample/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorExample
+{
+    public class Calculator
+    {
+        public int total;
+        public List<int> numbers = new List<int>();
+
+        public void Add(int number)
+        {
+            numbers.Add(number);
+        }
+
+        public int Sum()
+        {
+            return numbers.Sum();
+        }
+    }
+}
---
+++ CalculatorTests/Specs/CalculatorSteps.cs
@@ -0,0 +1,31 @@
+﻿using CalculatorExample;
+using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace CalculatorTests.Specs
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        public Calculator calculator = new Calculator();
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calculator.Add(number);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            calculator.Sum();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(calculator.Sum(), expectedResult);
+        }
+    }
+}
